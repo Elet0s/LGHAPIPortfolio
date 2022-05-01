ae786962b9c849dc01025ff1a0e162a04fd42ee3
Eletos <lthlovelyg@naver.com>
2022-05-02 01:52:57 +0900

    이관형 : 업데이트 오류 수정

--- Helltaker/GameEngineBase/GameEngineDirectory.h
+++ Helltaker/GameEngineBase/GameEngineDirectory.h
@@ -7,9 +7,7 @@ class GameEngineDirectory : public GameEnginePath
 public:
 	// constrcuter destructer
 	GameEngineDirectory();
-	GameEngineDirectory(const char* _Path);
-	GameEngineDirectory(std::filesystem::path _Path);
-	GameEngineDirectory(const GameEngineDirectory& _Other);
+	GameEngineDirectory(const std::string& _Path);
 	~GameEngineDirectory();
 
 	// delete Function
@@ -27,8 +25,6 @@ public:
 	void Move(const std::string& _Name);
 
 	std::vector<GameEngineFile> GetAllFile(const std::string& _Ext = "");
-
-	std::vector<GameEngineDirectory> GetAllDirectory();
 
 protected:
 
--- Helltaker/GameEngineBase/GameEngineFile.h
+++ Helltaker/GameEngineBase/GameEngineFile.h
@@ -1,22 +1,48 @@
 #pragma once
-#include"GameEnginePath.h"
+#include "GameEnginePath.h"
 
+enum class OpenMode
+{
+	Read,
+	Write,
+};
 
+// Ό³Έν :
 class GameEngineFile : public GameEnginePath
 {
 public:
+	// constrcuter destructer
 	GameEngineFile();
-	GameEngineFile(const std::string& _Path);
+	GameEngineFile(const char* _Path);
 	GameEngineFile(std::filesystem::path _Path);
 	GameEngineFile(const GameEngineFile& _Other);
 	~GameEngineFile();
 
-	//GameEngineFile(const GameEngineFile& _Other) = delete;
-	//GameEngineFile(GameEngineFile&& _Other) noexcept = delete;
-	GameEngineFile& operator=(const GameEngineFile& _Other) = delete;
-	GameEngineFile& operator=(GameEngineFile&& _Other) noexcept = delete;
+	// delete Function
+	void Open(OpenMode _Mode);
+
+	void ReadOpen()
+	{
+		Open(OpenMode::Read);
+	}
+
+	void WriteOpen()
+	{
+		Open(OpenMode::Write);
+	}
+
+	void Close();
+
+	void Create()
+	{
+		Open(OpenMode::Write);
+		Close();
+	}
+
 protected:
 
-private:
 
+private:
+	FILE* FilePtr;
 };
+
